d7d00322e54bdffd5510bfacfa604118e2e10590
Shamitha Chandrasena <shamitha.chandrasena@mvv.se>
2019-10-31 11:54:31 +0530

    included end point to un secured web app

--- src/main/java/com/samsl/webauth/controllers/AuthenticationController.java
+++ src/main/java/com/samsl/webauth/controllers/AuthenticationController.java
@@ -3,6 +3,7 @@ package com.samsl.webauth.controllers;
 import com.samsl.onemarketsl.characters.User;
 import com.samsl.security.sevices.AuthenticationService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,12 +13,12 @@ import org.springframework.web.bind.annotation.RestController;
 public class AuthenticationController extends com.samsl.security.controllers.AuthenticationController{
 
     @RequestMapping(value = "/login", method = RequestMethod.POST)
-    public String authenticateUser(User user) {
+    public String authenticateUser(@RequestBody User user) {
         return authenticate(user);
     }
 
     @RequestMapping(value = "/register", method = RequestMethod.POST)
-    public User registerUser(User user) {
+    public User registerUser(@RequestBody User user) {
         return register(user);
     }
 
--- src/main/java/com/samsl/webauth/services/UserService.java
+++ src/main/java/com/samsl/webauth/services/UserService.java
@@ -2,26 +2,52 @@ package com.samsl.webauth.services;
 
 import com.samsl.onemarketsl.characters.User;
 import org.jvnet.hk2.annotations.Service;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.*;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.List;
 
 @Service
 @Component
 public class UserService implements com.samsl.onemarketsl.services.UserService{
+
+    RestTemplate restTemplate = new RestTemplate();
+
+
+
     @Override
     public User addUser(User user) {
-        return null;
+        String uri = "http://localhost:8080/onemarketsl/webapi/users/register";
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("Accept", "*/*");
+        HttpEntity<?> entity = new HttpEntity<>(user,headers);
+        return restTemplate.postForObject(uri,entity,User.class,new Object());
     }
 
     @Override
     public List<User> getAllUsers() {
-        return null;
+        String uri = "http://localhost:8080/onemarketsl/webapi/users/all";
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("Accept", MediaType.APPLICATION_JSON_VALUE);
+        HttpEntity<?> entity = new HttpEntity<>(headers);
+        ResponseEntity<List<User>> response = restTemplate.exchange(
+                uri,
+                HttpMethod.GET,
+                null,
+                new ParameterizedTypeReference<List<User>>(){});
+        List<User> users = response.getBody();
+        return users;
     }
 
     @Override
-    public User getUserById(int i) {
-        return null;
+    public User getUserById(int id) {
+        String uri = "http://localhost:8080/onemarketsl/webapi/users?id="+id;
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("Accept", MediaType.APPLICATION_JSON_VALUE);
+        HttpEntity<?> entity = new HttpEntity<>(headers);
+        return restTemplate.getForObject(uri,User.class);
     }
 
     @Override
